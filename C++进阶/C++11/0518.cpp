40f4e21a225ad4aa83726dff66d4c27ba88a54da
Allen <2439506288@qq.com>
2022-05-19 09:13:10 +0800

    2002-05-19  0518.cpp
    Signed-off-by: Allen <2439506288@qq.com>

--- C++进阶/C++11/0518.cpp
+++ C++进阶/C++11/0518.cpp
@@ -189,188 +189,193 @@ using namespace std;
 //}
 
 
-namespace allen
-{
-	class string
-	{
-	public:
-		typedef char* iterator;
-		iterator begin()
-		{
-			return _str;
-		}
-
-		iterator end()
-		{
-			return _str + _size;
-		}
-
-		string(const char* str = "")
-			:_size(strlen(str))
-			, _capacity(_size)
-		{
-			//cout << "string(char* str)" << endl;
-
-			_str = new char[_capacity + 1];
-			strcpy(_str, str);
-		}
-
-		// s1.swap(s2)
-		void swap(string& s)
-		{
-			::swap(_str, s._str);
-			::swap(_size, s._size);
-			::swap(_capacity, s._capacity);
-		}
-
-		// 拷贝构造
-		string(const string& s)
-			:_str(nullptr)
-		{
-			cout << "string(const string& s) -- 深拷贝" << endl;
-
-			string tmp(s._str);
-			swap(tmp);
-		}
-
-		// 移动构造
-		string(string&& s)
-			:_str(nullptr)
-			, _size(0)
-			, _capacity(0)
-		{
-			cout << "string(string&& s) -- 移动构造" << endl;
-			this->swap(s);
-		}
-
-		// 移动赋值 
-		string& operator=(string&& s)
-		{
-			cout << "string& operator=(string&& s) -- 移动赋值" << endl;
-			this->swap(s);
-
-			return *this;
-		}
-
-		// 赋值重载
-		string& operator=(const string& s)
-		{
-			cout << "string& operator=(string s) -- 深拷贝" << endl;
-			string tmp(s);
-			swap(tmp);
-
-			return *this;
-		}
-
-		~string()
-		{
-			delete[] _str;
-			_str = nullptr;
-		}
-
-		char& operator[](size_t pos)
-		{
-			assert(pos < _size);
-			return _str[pos];
-		}
-
-		void reserve(size_t n)
-		{
-			if (n > _capacity)
-			{
-				char* tmp = new char[n + 1];
-				strcpy(tmp, _str);
-				delete[] _str;
-				_str = tmp;
-
-				_capacity = n;
-			}
-		}
-
-		void push_back(char ch)
-		{
-			if (_size >= _capacity)
-			{
-				size_t newcapacity = _capacity == 0 ? 4 : _capacity * 2;
-				reserve(newcapacity);
-			}
-
-			_str[_size] = ch;
-			++_size;
-			_str[_size] = '\0';
-		}
-
-		//string operator+=(char ch)
-		string& operator+=(char ch)
-		{
-			push_back(ch);
-			return *this;
-		}
-
-		const char* c_str() const
-		{
-			return _str;
-		}
-	private:
-		char* _str;
-		size_t _size;
-		size_t _capacity; // 不包含最后做标识的\0
-	};
-
-	allen::string to_string(int value)
-	{
-		bool flag = true;
-		if (value < 0)
-		{
-			flag = false;
-			value = 0 - value;
-		}
-
-		allen::string str;
-		while (value > 0)
-		{
-			int x = value % 10;
-			value /= 10;
-
-			str += ('0' + x);
-		}
-
-		if (flag == false)
-		{
-			str += '-';
-		}
-
-		std::reverse(str.begin(), str.end());
-		return str;
-	}
-}
+//namespace allen
+//{
+//	class string
+//	{
+//	public:
+//		typedef char* iterator;
+//		iterator begin()
+//		{
+//			return _str;
+//		}
+//
+//		iterator end()
+//		{
+//			return _str + _size;
+//		}
+//
+//		string(const char* str = "")
+//			:_size(strlen(str))
+//			, _capacity(_size)
+//		{
+//			//cout << "string(char* str)" << endl;
+//
+//			_str = new char[_capacity + 1];
+//			strcpy(_str, str);
+//		}
+//
+//		// s1.swap(s2)
+//		void swap(string& s)
+//		{
+//			::swap(_str, s._str);
+//			::swap(_size, s._size);
+//			::swap(_capacity, s._capacity);
+//		}
+//
+//		// 拷贝构造
+//		string(const string& s)
+//			:_str(nullptr)
+//		{
+//			cout << "string(const string& s) -- 深拷贝" << endl;
+//
+//			string tmp(s._str);
+//			swap(tmp);
+//		}
+//
+//		// 移动构造
+//		string(string&& s)
+//			:_str(nullptr)
+//			, _size(0)
+//			, _capacity(0)
+//		{
+//			cout << "string(string&& s) -- 移动构造" << endl;
+//			this->swap(s);
+//		}
+//
+//		// 移动赋值 
+//		string& operator=(string&& s)
+//		{
+//			cout << "string& operator=(string&& s) -- 移动赋值" << endl;
+//			this->swap(s);
+//
+//			return *this;
+//		}
+//
+//		// 赋值重载
+//		string& operator=(const string& s)
+//		{
+//			cout << "string& operator=(string s) -- 深拷贝" << endl;
+//			string tmp(s);
+//			swap(tmp);
+//
+//			return *this;
+//		}
+//
+//		~string()
+//		{
+//			delete[] _str;
+//			_str = nullptr;
+//		}
+//
+//		char& operator[](size_t pos)
+//		{
+//			assert(pos < _size);
+//			return _str[pos];
+//		}
+//
+//		void reserve(size_t n)
+//		{
+//			if (n > _capacity)
+//			{
+//				char* tmp = new char[n + 1];
+//				strcpy(tmp, _str);
+//				delete[] _str;
+//				_str = tmp;
+//
+//				_capacity = n;
+//			}
+//		}
+//
+//		void push_back(char ch)
+//		{
+//			if (_size >= _capacity)
+//			{
+//				size_t newcapacity = _capacity == 0 ? 4 : _capacity * 2;
+//				reserve(newcapacity);
+//			}
+//
+//			_str[_size] = ch;
+//			++_size;
+//			_str[_size] = '\0';
+//		}
+//
+//		//string operator+=(char ch)
+//		string& operator+=(char ch)
+//		{
+//			push_back(ch);
+//			return *this;
+//		}
+//
+//		const char* c_str() const
+//		{
+//			return _str;
+//		}
+//	private:
+//		char* _str;
+//		size_t _size;
+//		size_t _capacity; // 不包含最后做标识的\0
+//	};
+//
+//	allen::string to_string(int value)
+//	{
+//		bool flag = true;
+//		if (value < 0)
+//		{
+//			flag = false;
+//			value = 0 - value;
+//		}
+//
+//		allen::string str;
+//		while (value > 0)
+//		{
+//			int x = value % 10;
+//			value /= 10;
+//
+//			str += ('0' + x);
+//		}
+//
+//		if (flag == false)
+//		{
+//			str += '-';
+//		}
+//
+//		std::reverse(str.begin(), str.end());
+//		return str;
+//	}
+//}
 
 // 左值引用的使用场景
 // 1、做参数 
 // 2、做返回值
-void func1(allen::string s)
-{}
+//void func1(allen::string s)
+//{}
+//
+//void func2(const allen::string& s)
+//{}
 
-void func2(const allen::string& s)
-{}
-
-int main()
-{
-	allen::string s1("hello world");
-	// func1和func2的调用我们可以看到左值引用做参数减少了拷贝，提高效率的使用场景和价值
-	//func1(s1);
-	func2(s1);
-
-	// operator+=可以使用传左值引用返回
-	s1 += 'A';
-
-	// to_string 不能用左值引用返回，这个就是左值引用短板
-	// 如果函数返回对象除了函数作用域就不在了，就不能使用做引用返回，就会存在拷贝
-	allen::string ret1 = allen::to_string(1234);
-	allen::to_string(1234);
-
-	return 0;
-}
+//int main()
+//{
+//	//allen::string s1("hello world");
+//	//// func1和func2的调用我们可以看到左值引用做参数减少了拷贝，提高效率的使用场景和价值
+//	////func1(s1);
+//	//func2(s1);
+//
+//	//// operator+=可以使用传左值引用返回
+//	//s1 += 'A';
+//
+//	//// to_string 不能用左值引用返回，这个就是左值引用短板
+//	//// 如果函数返回对象除了函数作用域就不在了，就不能使用做引用返回，就会存在拷贝
+//	//allen::string ret1 = allen::to_string(1234);
+//	//allen::to_string(1234);
+//
+//
+//	allen::string s1("hello world");
+//	allen::string s2= s1;
+//	allen::string s3 = allen::to_string(1234);
+//	allen::string s4 = std::move(s1);
+//	return 0;
+//}
 
 //int main()
 //{
